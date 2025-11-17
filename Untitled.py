689e6f85cc50ae574791bf61d115c7e915265196
keinless <chlgurwo99@gmail.com>
2025-11-17 13:53:01 +0900

    Add python script version

---
+++ Untitled.py
@@ -0,0 +1,35 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.18.1
+#   kernelspec:
+#     display_name: chj (3.13.5)
+#     language: python
+#     name: chj
+# ---
+
+# %%
+def add_numbers(a, b):
+    return a + b
+
+print(add_numbers(2, 3))
+
+# %%
+git clone https://github.com/chlgurwo99@gmail.com/test-vibe-coding.git
+# cd test-vibe-coding
+
+# %%
+# 예: 새로운 함수 추가
+def multiply_numbers(a, b):
+    return a * b
+
+
+# %%
+git add .
+git commit -m "Add multiply function"
+git push
